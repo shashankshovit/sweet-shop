f7aa805d3ad03895ced1d695ec6247dc2f7a5944
Shashank Shovit <shashankshovit@gmail.com>
2020-05-20 12:50:33 +0530

    Bug fixes: css; github link

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -2,6 +2,8 @@ class Footer extends BaseHTML {
 	constructor(html) {
 		super();
 		this.html = html;
-		this.add(new Container({inner: `<h1>Footer Here</h1>`}));
+		this.add(new Container({
+			className: 'sweet-shop-footer',
+			inner: '<h2><a href="https://github.com/shashankshovit/sweet-shop/">Link to Github repo</a></h2>'}));
 	}
 }
